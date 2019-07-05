d8ef25bcebc6b4b5beba2cb3351f204019edacbe
Amir Sohail <a.sohail@reply.it>
2019-07-05 16:52:49 +0200

    Refactor

--- inversion/src/main/java/com/siva/inversion/InversionApplication.java
+++ inversion/src/main/java/com/siva/inversion/InversionApplication.java
@@ -3,10 +3,6 @@ package com.siva.inversion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 @SpringBootApplication
 public class InversionApplication {
 
